a088d1fd87ee1419a70ea884f62db87229346f56
Cristian Crețu <crisemcr@gmail.com>
2022-07-09 13:07:57 +0300

    feat: search styling

--- components/BoostsView/BoostsView.tsx
+++ components/BoostsView/BoostsView.tsx
@@ -43,23 +43,29 @@ export default function BoostsView({ boosts }: BoostsViewProps) {
       {/* sidebar */}
       <div className='flex flex-col flex-shrink-0 sticky mr-8'>
         <form
-          className='rounded-md my-2'
-          onSubmit={event => {
-            event.preventDefault()
-          }}
+          className='relative rounded-md mb-4'
+          onSubmit={event => event.preventDefault()}
         >
-          <SearchIcon />
+          <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+            <SearchIcon aria-hidden='true' />
+          </div>
           <input
             type='text'
             value={input}
-            className={cn()}
+            // className={cn()}
+            // className='block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100'
+            className={cn(
+              'bg-gray-50 dark:bg-gray-800 text-primary block w-full pl-10 py-2 text-sm rounded-md',
+              'border border-gray-200 dark:border-gray-700',
+              'pr-0 md:pr-12'
+            )}
             placeholder='Search for boosts...'
-            aria-label='Search for links or commands'
+            aria-label='Search for boosts'
             onChange={handleChange}
             spellCheck={false}
             autoComplete='off'
           />
-          <div className='hidden md:flex absolute inset-y-0 right-0 py-1.5 pr-1.5 pointer-events-none'>
+          <div className='hidden md:flex absolute inset-y-0 right-0 py-2 pr-2 pointer-events-none text-tertiary'>
             <kbd>⌘K</kbd>
           </div>
         </form>
--- components/SearchIcon/SearchIcon.tsx
+++ components/SearchIcon/SearchIcon.tsx
@@ -1,19 +1,18 @@
 export default function SearchIcon() {
   return (
     <svg
-      width='15'
-      height='15'
-      viewBox='0 0 15 15'
-      fill='currentColor'
+      className='w-5 h-5 text-gray-400 dark:text-gray-300'
       xmlns='http://www.w3.org/2000/svg'
-      className='text-gray-800 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-100'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
     >
       <path
-        d='M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z'
-        fill='currentColor'
-        fillRule='evenodd'
-        clipRule='evenodd'
-      ></path>
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+      />
     </svg>
   )
 }
