d0963b4380c76414d9b29b6615bf244fe9dda13e
Cristian Crețu <crisemcr@gmail.com>
2022-10-08 10:06:54 +0300

    feat: add link

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,14 +14,22 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Container>
-      <div className='flex flex-col itmes-center mx-auto'>
+      <div className='flex flex-col mx-auto itmes-center'>
         <header className='flex flex-col items-center p-2 md:p-24 '>
-          <h1 className='text-4xl sm:text-5xl lg:text-7xl font-bold text-primary'>
+          <h1 className='text-4xl font-bold sm:text-5xl lg:text-7xl text-primary'>
             Unofficial Marketplace
           </h1>
           <h2 className='text-4xl sm:text-5xl lg:text-7xl font-bold animate-text-shimmer bg-clip-text text-blue-500/40 dark:text-blue-700/60 bg-[linear-gradient(110deg,#e2e8f0,45%,#1e293b,55%,#e2e8f0)] bg-[length:250%_100%]'>
             for Arc Boosts
           </h2>
+          <a
+            href='https://github.com/cristicretu/arcboost-marketplace/blob/main/README.md#downloading'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='mt-4 text-gray-400 transition-all duration-200 dark:text-gray-500 hover:text-gray-600 dark:hover:text-gray-300'
+          >
+            How to install a boost?
+          </a>
         </header>
         <BoostsView boosts={boosts} />
         <Footer />
